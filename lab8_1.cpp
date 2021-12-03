51199132ec4879c4cfa2105e73f1c5fc60401bf5
Nuttadon <nuttadon_p@cmu.ac.th>
2021-12-03 10:50:19 +0700

    Lab8_1 complete

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+    int a  =  20,b = 30,c = 4 ;
+    double output;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+    output= a-(double)b/c;//ทำก่อน=เปลี่ยนก่อน หรือ ถ้าเปลี่ยนส่วนที่ทำก่อนเป็นdoubleทั้งสมการจะกลายเป็นdouble
+    cout << "output:" << output << "\n";
     return 0;
 }
