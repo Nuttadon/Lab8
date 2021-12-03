03d76d9acfa05119281cff0d01e36969c32c47d4
Nuttadon <nuttadon_p@cmu.ac.th>
2021-12-03 12:16:40 +0700

    Lab8_3 complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
 using namespace std;
-
+// A65-Z90
+//a97-z112
 char before(char x){
-	//Write your function definition here
+	char y='0';
+	if(64<x&&x<91){
+		if(x>65){
+			return x-1;
+		}
+		if(x==65){
+			return x+25;
+		}
+	}
+	return y; //ถ้า return เป็น string หรือ char ที่ว่างเปล่า จะติด \x00 << หมายถึงเครื่องหมายเพื่อให้คอมหยุดอ่านstring นั้น
 }
-
 int main(){
 	//Test Case
 	cout << before('A') << "\n";
